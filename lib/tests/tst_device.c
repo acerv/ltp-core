c99c6e4a76742ad45f562f55dbf6780f79a4236f
Andrea Cervesato <andrea.cervesato@suse.com>
2023-11-10 10:46:20 +0100

    Delete old library tests
    Old library tests have been deleted and new library tests will replace
    them.

--- lib/tests/tst_device.c
+++ lib/tests/tst_device.c
@@ -1,53 +1,112 @@
+// SPDX-License-Identifier: GPL-2.0-or-later
 /*
- * Copyright (C) 2012 Cyril Hrubis <chrubis@suse.cz>
- *
- * This program is free software; you can redistribute it and/or modify it
- * under the terms of version 2 of the GNU General Public License as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it would be useful, but
- * WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
- *
- * Further, this software is distributed without any warranty that it is
- * free of the rightful claim of any third person regarding infringement
- * or the like.  Any license provided herein, whether implied or
- * otherwise, applies only to this software file.  Patent licenses, if
- * any, provided herein do not apply to combinations of this program with
- * other software, or any other product whatsoever.
- *
- * You should have received a copy of the GNU General Public License along
- * with this program; if not, write the Free Software Foundation, Inc.,
- * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ * Copyright (c) 2016 Linux Test Project
+ * Copyright (C) 2021 SUSE LLC Andrea Cervesato <andrea.cervesato@suse.com>
  */
+#define _GNU_SOURCE
 
-#include "test.h"
+#include <stdlib.h>
+#include <sys/mount.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <lapi/loop.h>
+#include <time.h>
 
-char *TCID = "tst_device";
-int TST_TOTAL = 1;
+#include "tst_test.h"
 
-static const char *dev;
+#define DEVBLOCKSIZE 2048
+#define DEV_MIN_SIZE 310
+
+static char *mntpoint;
+static uint64_t ltp_dev_size;
+
+static int set_block_size(int fd)
+{
+	return ioctl(fd, LOOP_SET_BLOCK_SIZE, DEVBLOCKSIZE);
+}
+
+static void setup(void)
+{
+	int fd;
+	int ret;
+
+	ret = asprintf(&mntpoint, "%s/mnt", tst_get_tmpdir());
+	if (ret < 0)
+		tst_brk(TBROK, "asprintf failure");
+
+	fd = SAFE_OPEN(tst_device->dev, O_RDONLY);
+
+	SAFE_IOCTL(fd, BLKGETSIZE64, &ltp_dev_size);
+
+	TST_RETRY_FN_EXP_BACKOFF(set_block_size(fd), TST_RETVAL_EQ0, 10);
+
+	SAFE_CLOSE(fd);
+
+	SAFE_MKFS(tst_device->dev, tst_device->fs_type, NULL, NULL);
+
+	SAFE_MKDIR(mntpoint, 0777);
+	SAFE_MOUNT(tst_device->dev, mntpoint, tst_device->fs_type, 0, 0);
+}
 
 static void cleanup(void)
 {
-	if (dev)
-		tst_release_device(dev);
-
-	tst_rmdir();
+	if (tst_is_mounted(mntpoint))
+		SAFE_UMOUNT(mntpoint);
 }
 
-int main(void)
+static void test_dev_min_size(void)
 {
-	tst_tmpdir();
+	uint64_t size;
 
-	dev = tst_acquire_device(cleanup);
-	if (!dev)
-		tst_brkm(TCONF, cleanup, "Failed to acquire test device");
+	size = ltp_dev_size / 1024 / 1024;
 
-	printf("Test device='%s'\n", dev);
-
-	tst_mkfs(cleanup, dev, "ext2", NULL, NULL);
-
-	cleanup();
-	tst_exit();
+	if (size == DEV_MIN_SIZE)
+		tst_res(TPASS, "Got expected device size %lu", size);
+	else
+		tst_res(TFAIL, "Expected device size is %d but got %lu",
+			DEV_MIN_SIZE, size);
 }
+
+static void test_tst_find_backing_dev(void)
+{
+	char block_dev[100];
+
+	tst_find_backing_dev(mntpoint, block_dev, sizeof(block_dev));
+
+	if (!strcmp(tst_device->dev, block_dev))
+		tst_res(TPASS, "%s belongs to %s block dev", mntpoint,
+			block_dev);
+	else
+		tst_res(TFAIL, "%s should belong to %s, but %s is returned",
+			mntpoint, tst_device->dev, block_dev);
+}
+
+static void test_tst_dev_block_size(void)
+{
+	int block_size;
+
+	block_size = tst_dev_block_size(mntpoint);
+
+	if (block_size == DEVBLOCKSIZE)
+		tst_res(TPASS, "%s has %d block size", mntpoint, block_size);
+	else
+		tst_res(TFAIL, "%s has %d block size, but expected is %i",
+			mntpoint, block_size, DEVBLOCKSIZE);
+}
+
+static void do_test(void)
+{
+	test_dev_min_size();
+	test_tst_find_backing_dev();
+	test_tst_dev_block_size();
+}
+
+static struct tst_test test = {
+	.needs_root = 1,
+	.needs_device = 1,
+	.dev_min_size = DEV_MIN_SIZE,
+	.test_all = do_test,
+	.setup = setup,
+	.cleanup = cleanup,
+	.min_kver = "4.14",
+};
